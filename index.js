a299339e1bae48e4b46ae109259239e56443a1a8
kazukikanomata <kazu.surface.pro2020@outlook.jp>
2023-01-03 16:09:11 +0900

    add：テンプレート文字列isuseful

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ arr1.push("money");
 console.log(arr1);
 
 // テンプレート文字列の使用を可能にした
+const first_name = "kazuki";
+const age = 24;
+const message = `私の名前は${first_name}です。年齢は${age}です。`;
+console.log(message);
