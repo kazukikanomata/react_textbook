f64e1cedbf6d49370cd42d2ad5d2ed6ab205d383
kazukikanomata <kazu.surface.pro2020@outlook.jp>
2023-01-03 16:28:14 +0900

    add：アロー関数書き方注意

--- index.js
+++ index.js
@@ -18,9 +18,34 @@ function func1(value){
 console.log(func1("func1です"));
 
 // アロー関数
-const func2 = (value) =>{
-    return value;
-}
+// const func2 = (value) =>{
+//     return value;
+// }
+// console.log(func2("func2です。"));
 
+// 注意
+
+// 引数が1つのときはかっこを省略することができる
+const func2 = value =>{
+    return value;
+};
 console.log(func2("func2です。"));
 
+// 引数が2つ以上だとエラー
+// ちゃんと（）でくくりましょう
+// const func3 = value1, value2 =>{
+//     return value1 + value2;
+// };
+
+// 処理が単一ぎょうのときは省略(括弧とreturn)できるのは良い
+const func4 = (num1, num2) => num1 + num2;
+console.log(func4(10,20));
+
+// 返却値が複数行に及ぶ場合は()で囲むことで単一行のようにまとめて返却することができる。
+const func5 = (val1, val2)=>(
+    {
+        first_name1: val1,
+        age: val2,
+    }
+)
+console.log(func5("kazuki", 24));
